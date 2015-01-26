2059c33b0cd407fda45b7a6877906ec8e98b4d63
thbe <project@bendler-net.de>
2015-01-26 22:03:54 +0100

    Added tvshows

--- videodb_to_html.rb
+++ videodb_to_html.rb
@@ -7,9 +7,6 @@
 # Date:   Fri Jan 23 18:50:37 CET 2015
 #
 
-# TODO tv series need to be catched as well
-# TODO style sheet settings for img tag
-
 # Include required libraries
 require 'nokogiri'
 require 'open-uri'
@@ -91,6 +88,9 @@ doc.xpath('//movie').each do |movie|
   if poster_url.to_s.strip.length == 0
     poster_url = "No information available"
   end
+  movie_array[movie_counter] = [title, country, director, studio, year, runtime, story, poster_url]
+  movie_counter += 1
+  # --- Debug ---
   # puts "Titel:      " + title
   # puts "Country:    " + country
   # puts "Director:   " + director
@@ -100,80 +100,74 @@ doc.xpath('//movie').each do |movie|
   # puts "Story:      " + story
   # puts "Poster URL: " + poster_url
   # puts ""
-  movie_array[movie_counter][0] = title
-  movie_array[movie_counter][1] = country
-  movie_array[movie_counter][2] = director
-  movie_array[movie_counter][3] = studio
-  movie_array[movie_counter][4] = year
-  movie_array[movie_counter][5] = runtime
-  movie_array[movie_counter][6] = story
-  movie_array[movie_counter][7] = poster_url
-  movie_counter += 1
 end
 
-# Initialize and populate tv_series array
-# puts "Generate tv_series array ..."
-# tv_series_array = Array.new(doc.xpath('//tv_series').count) { Array.new(7) }
-# tv_series_counter = 0
-# doc.xpath('//tv_series').each do |tv_series|
-#   if !tv_series.at_xpath('title').nil?
-#     title = tv_series.at_xpath('title').content
-#   end
-#   if !tv_series.at_xpath('country').nil?
-#     country = tv_series.at_xpath('country').content
-#   end
-#   if !tv_series.at_xpath('director').nil?
-#     director = tv_series.at_xpath('director').content
-#   end
-#   if !tv_series.at_xpath('studio').nil?
-#     studio = tv_series.at_xpath('studio').content
-#   end
-#   if !tv_series.at_xpath('year').nil?
-#     year = tv_series.at_xpath('year').content
-#   end
-#   if !tv_series.at_xpath('runtime').nil?
-#     runtime = tv_series.at_xpath('runtime').content + " minutes"
-#   end
-#   if !tv_series.at_xpath('outline').nil?
-#     story = tv_series.at_xpath('outline').content
-#   end
-#   if !tv_series.at_xpath('thumb').nil?
-#     poster_url = tv_series.at_xpath('thumb').content
-#   end
-#   if title.to_s.strip.length == 0
-#     title = "No information available"
-#   end
-#   if country.to_s.strip.length == 0
-#     country = "No information available"
-#   end
-#   if director.to_s.strip.length == 0
-#     director = "No information available"
-#   end
-#   if studio.to_s.strip.length == 0
-#     studio = "No information available"
-#   end
-#   if year.to_s.strip.length == 0
-#     year = "No information available"
-#   end
-#   if runtime.to_s.strip.length == 0
-#     runtime = "No information available"
-#   end
-#   if story.to_s.strip.length == 0
-#     story = "No information available"
-#   end
-#   if poster_url.to_s.strip.length == 0
-#     poster_url = "No information available"
-#   end
-#   tv_series_array[tv_series_counter][0] = title
-#   tv_series_array[tv_series_counter][1] = country
-#   tv_series_array[tv_series_counter][2] = director
-#   tv_series_array[tv_series_counter][3] = studio
-#   tv_series_array[tv_series_counter][4] = year
-#   tv_series_array[tv_series_counter][5] = runtime
-#   tv_series_array[tv_series_counter][6] = story
-#   tv_series_array[tv_series_counter][7] = poster_url
-#   tv_series_counter += 1
-# end
+# Initialize and populate tvshow array
+puts "Generate tvshow array ..."
+tvshow_array = Array.new(doc.xpath('//tvshow/episodedetails').count) { Array.new(7) }
+tvshow_counter = 0
+doc.xpath('//tvshow/episodedetails').each do |tvshow|
+  if !tvshow.at_xpath('title').nil?
+    title = tvshow.at_xpath('title').content
+  end
+  if !tvshow.at_xpath('showtitle').nil?
+    showtitle = tvshow.at_xpath('showtitle').content
+  end
+  if !tvshow.at_xpath('season').nil?
+    season = tvshow.at_xpath('season').content
+  end
+  if !tvshow.at_xpath('episode').nil?
+    episode = tvshow.at_xpath('episode').content
+  end
+  if !tvshow.at_xpath('year').nil?
+    year = tvshow.at_xpath('year').content
+  end
+  if !tvshow.at_xpath('runtime').nil?
+    runtime = tvshow.at_xpath('runtime').content + " minutes"
+  end
+  if !tvshow.at_xpath('plot').nil?
+    story = tvshow.at_xpath('plot').content
+  end
+  if !tvshow.at_xpath('thumb').nil?
+    poster_url = tvshow.at_xpath('thumb').content
+  end
+  if title.to_s.strip.length == 0
+    title = "No information available"
+  end
+  if showtitle.to_s.strip.length == 0
+    showtitle = "No information available"
+  end
+  if season.to_s.strip.length == 0
+    season = "No information available"
+  end
+  if episode.to_s.strip.length == 0
+    episode = "No information available"
+  end
+  if year.to_s.strip.length == 0
+    year = "No information available"
+  end
+  if runtime.to_s.strip.length == 0
+    runtime = "No information available"
+  end
+  if story.to_s.strip.length == 0
+    story = "No information available"
+  end
+  if poster_url.to_s.strip.length == 0
+    poster_url = "No information available"
+  end
+  tvshow_array[tvshow_counter] = [title, showtitle, season, episode, year, runtime, story, poster_url]
+  tvshow_counter += 1
+  # --- Debug ---
+  # puts "TV-Show:    " + showtitle
+  # puts "Title:      " + title
+  # puts "Season:     " + season
+  # puts "Episode:    " + episode
+  # puts "Year:       " + year
+  # puts "Runtime:    " + runtime
+  # puts "Story:      " + story
+  # puts "Poster URL: " + poster_url
+  # puts ""
+end
 
 # Generate picture cache (skip if image already exists)
 puts "Generate picture cache ..."
@@ -223,6 +217,39 @@ movie_array.each do |row|
   print download_status
   movie_counter += 1
 end
+tvshow_counter = 0
+tvshow_array.each do |row|
+  url = row[7]
+  tvshow_array[tvshow_counter][7] = "cache/blank.gif"
+  download_status = " - failed\n"
+  print tvshow_array[tvshow_counter][0]
+  print " - "
+  print url
+  if url != "No information available"
+    url_parsed = URI.parse(url)
+    tvshow_picture_host = url_parsed.host
+    tvshow_picture_name = File.basename(url_parsed.path)
+    tvshow_picture_filename = "cache/" + tvshow_picture_name
+  end
+  if tvshow_picture_host.to_s.strip.length != 0
+    if ! File.exists?(tvshow_picture_filename)
+      if host_name_exist(tvshow_picture_host)
+        return_code = Net::HTTP.get_response(URI.parse(url.to_s)).code
+        if return_code == "200" or return_code == "301" or return_code == "302"
+          tvshow_picture_content = open(url).read
+          File.open(tvshow_picture_filename, 'w'){|file| file.write(tvshow_picture_content)}
+          tvshow_array[tvshow_counter][7] = tvshow_picture_filename
+          download_status = " - done\n"
+        end
+      end
+    else
+      tvshow_array[tvshow_counter][7] = tvshow_picture_filename
+      download_status = " - skipped, image already in cache\n"
+    end
+  end
+  print download_status
+  tvshow_counter += 1
+end
 
 # Generate HTML file out of movie grid
 puts "Generate HTML output ..."
@@ -271,7 +298,7 @@ video_html_file.puts "    }"
 video_html_file.puts "  </style>"
 video_html_file.puts "</head>"
 video_html_file.puts "<body>"
-video_html_file.puts "  <h1>Kodi Mediacenter Movie Content List</h1>"
+video_html_file.puts "  <h1 id=\"kodi\">Kodi Mediacenter Movie Content List</h1>"
 video_html_file.puts "  <table id=\"kodi\">"
 video_html_file.puts "    <tr>"
 video_html_file.puts "      <th width=\"10%\">Poster</th>"
@@ -299,7 +326,34 @@ movie_array.each do |row|
   end
 end
 video_html_file.puts "  </table>"
-video_html_file.puts "  <h1>Kodi Mediacenter TV Series Content List</h1>"
+video_html_file.puts "  <h1 id=\"kodi\">Kodi Mediacenter TV Series Content List</h1>"
+video_html_file.puts "  <table id=\"kodi\">"
+video_html_file.puts "    <tr>"
+video_html_file.puts "      <th width=\"10%\">Poster</th>"
+video_html_file.puts "      <th width=\"25%\">Information</th>"
+video_html_file.puts "      <th>Story</th>"
+video_html_file.puts "    </tr>"
+table_row_format = "    <tr>"
+tvshow_array.each do |row|
+  video_html_file.puts table_row_format
+  video_html_file.puts "      <td><img src=\"#{row[7]}\"></td>"
+  video_html_file.puts "      <td>"
+  video_html_file.puts "        TV-Show: #{row[0]}<br />"
+  video_html_file.puts "        Title: #{row[1]}<br />"
+  video_html_file.puts "        Season: #{row[2]}<br />"
+  video_html_file.puts "        Episode: #{row[3]}<br />"
+  video_html_file.puts "        Year: #{row[4]}<br />"
+  video_html_file.puts "        Runtime: #{row[5]}<br />"
+  video_html_file.puts "      </td>"
+  video_html_file.puts "      <td>#{row[6]}</td>"
+  video_html_file.puts "    </tr>"
+  if table_row_format == "    <tr>"
+    table_row_format = "    <tr class=\"alt\">"
+  else
+    table_row_format = "    <tr>"
+  end
+end
+video_html_file.puts "  </table>"
 video_html_file.puts "</body>"
 video_html_file.puts "</html>"
 video_html_file.close()
